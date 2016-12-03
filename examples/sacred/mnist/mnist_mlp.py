1beabfd26064587dc1e96c181b62f8d130ac8095
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2016-12-03 22:18:42 +0700

    Refactor the model and use Sacred ingredients to modularize the code.

--- examples/sacred/mnist/mnist_mlp.py
+++ examples/sacred/mnist/mnist_mlp.py
@@ -13,30 +13,28 @@ This is a basic example how to wrap the code as Sacred Experiment
 """
 
 from keras.datasets import mnist
-from keras.models import Sequential
 from keras.layers.core import Dense, Dropout, Activation
+from keras.models import Sequential
 from keras.utils import np_utils
-from sacred import Experiment
+from sacred import Experiment, Ingredient
 
-ex = Experiment('mnist')
+dataset_ingredient = Ingredient('dataset')
+net_ingredient = Ingredient('feed_forward_network')
 
-@ex.config
-def ex_config():
-    batch_size = 128
-    nb_classes = 10
-    nb_epoch = 20
+ex = Experiment('mnist', ingredients=[dataset_ingredient, net_ingredient])
 
-@ex.automain
-def main(batch_size, nb_classes, nb_epoch):
+@dataset_ingredient.capture
+def load_data(nb_classes=10):
     # the data, shuffled and split between train and test sets
     (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
-    X_train = X_train.reshape(60000, 784)
-    X_test = X_test.reshape(10000, 784)
-    X_train = X_train.astype('float32')
-    X_test = X_test.astype('float32')
-    X_train /= 255
-    X_test /= 255
+    def prepare_X(X):
+        X = X.reshape(X.shape[0], -1)
+        X = X.astype('float32')
+        X /= 255
+        return X
+
+    X_train, X_test = [prepare_X(X) for X in (X_train, X_test)]
     print(X_train.shape[0], 'train samples')
     print(X_test.shape[0], 'test samples')
 
@@ -44,14 +42,29 @@ def main(batch_size, nb_classes, nb_epoch):
     Y_train = np_utils.to_categorical(y_train, nb_classes)
     Y_test = np_utils.to_categorical(y_test, nb_classes)
 
+    return X_train, Y_train, X_test, Y_test
+
+@net_ingredient.config
+def net_ingredient_config():
+    input_shape=(784,)
+    nb_classes=10
+    nb_layers=3
+    layer_width=512
+    dropout=0.2
+    activation='relu'
+
+@net_ingredient.capture
+def create_model(input_shape, nb_classes, nb_layers, layer_width, dropout, activation):
     model = Sequential()
-    model.add(Dense(512, input_shape=(784,)))
-    model.add(Activation('relu'))
-    model.add(Dropout(0.2))
-    model.add(Dense(512))
-    model.add(Activation('relu'))
-    model.add(Dropout(0.2))
-    model.add(Dense(10))
+    for i in range(nb_layers - 1):
+        if i == 0:
+            model.add(Dense(layer_width, input_shape=input_shape))
+        else:
+            model.add(Dense(layer_width))
+        model.add(Activation(activation))
+        if dropout > 0:
+            model.add(Dropout(dropout))
+    model.add(Dense(nb_classes))
     model.add(Activation('softmax'))
 
     model.summary()
@@ -59,12 +72,23 @@ def main(batch_size, nb_classes, nb_epoch):
     model.compile(loss='categorical_crossentropy',
                   optimizer='adam',
                   metrics=['accuracy'])
+    return model
+
+@ex.config
+def ex_config():
+    batch_size = 128
+    nb_epoch = 20
+
+@ex.automain
+def main(batch_size, nb_epoch):
+    X_train, Y_train, X_test, Y_test = load_data()
+    model = create_model()
 
     history = model.fit(X_train, Y_train,
                         batch_size=batch_size, nb_epoch=nb_epoch,
                         verbose=1, validation_data=(X_test, Y_test))
     score = model.evaluate(X_test, Y_test, verbose=0)
-
+    
     return {
         'history': history.history,
         'test_loss': score[0],
