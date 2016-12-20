09b7ab7878ba35104f08f8f42c477e226a479bdb
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2016-12-20 14:15:28 +0700

    Create a setup.py.

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+setup(name='sanctuary',
+      version='0.1',
+      description='Managed machine-learning model training tool based on sacred.',
+      url='https://github.com/bzamecnik/sanctuary',
+      author='Bohumir Zamecnik',
+      author_email='bohumir.zamecnik@gmail.com',
+      zip_safe=False,
+      py_modules=['sanctuary'],
+      install_requires=[
+        'sacred',
+      ],
+      setup_requires=['setuptools-markdown'],
+      long_description_markdown_filename='README.md',
+      classifiers=[
+          # How mature is this project? Common values are
+          #   3 - Alpha
+          #   4 - Beta
+          #   5 - Production/Stable
+          'Development Status :: 3 - Alpha',
+
+          'Intended Audience :: Developers',
+          'Intended Audience :: Science/Research',
+
+          'License :: Other/Proprietary License',
+
+          'Programming Language :: Python :: 3',
+
+          'Operating System :: POSIX :: Linux',
+          'Operating System :: MacOS :: MacOS X',
+          ]
+      )
